43041f73af988357d92015a032a9acc184ab035f
Nattapol Chittrichat <thaidevelopers@gmail.com>
2018-11-16 15:15:52 +0700

    Initial Commit

---
+++ DemoDay1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoDay1
+//
+//  Created by Nattapol Chittrichat on 16/11/2561 BE.
+//  Copyright © 2561 Nattapol Chittrichat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
